974877d5149b3b6cdfc4038f120dbf64c6a03a0b
hanlee <me@hanlee.co>
2020-09-26 20:50:27 +0900

    feat: error page

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-full">
+    <div v-if="error.statusCode === 404" class="animate-appear">
+      <img class="" src="~/assets/image/rafiki.svg" alt="">
+    </div>
+    <h1 v-else>
+      An error occurred
+    </h1>
+    <nuxt-link class="btn mt-16" to="/">
+      Home page
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default () {
+        return { statusCode: 404 }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
