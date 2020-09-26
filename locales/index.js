e46cddf0444587f669cce08bf70fb8719d88ce64
hanlee <me@hanlee.co>
2020-09-27 00:21:32 +0900

    i18n: add locales

---
+++ locales/index.js
@@ -0,0 +1,22 @@
+import en from './en.json'
+import ja from './ja.json'
+
+export const messages = {
+  en,
+  ja
+}
+
+export const locales = [{
+  code: 'ja',
+  name: '日本語',
+  iso: 'ja-JP',
+  file: 'ja.json',
+  description: 'Japanese'
+},
+{
+  code: 'en',
+  name: 'English',
+  iso: 'en-US',
+  file: 'en.json',
+  description: 'English'
+}]
