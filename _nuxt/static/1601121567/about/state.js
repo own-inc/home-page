5fc3d40a68bf43f67eadb9b79b473e24b7b40258
hannoeru <hannoeru@users.noreply.github.com>
2020-09-26 11:59:29 +0000

    deploy: c7e5ad32d2061eb729b352f362a0331a0b2dc7d8

---
+++ _nuxt/static/1601121567/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1601121567",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{content:{dbHash:"3be24260"}}}}("\u002Fabout"));
