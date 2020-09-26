d68ae6d853d8464b10f15ac0cfe57762823a4c27
hanlee <me@hanlee.co>
2020-09-27 00:23:32 +0900

    feat: adopt mobile

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center h-full">
-    <div v-if="error.statusCode === 404" class="animate-appear">
-      <img class="" src="~/assets/image/rafiki.svg" alt="">
+    <div v-if="error.statusCode === 404" class="animate-appear w-2/3 md:w-1/4 flex justify-center">
+      <img src="~/assets/image/rafiki.svg" alt="">
     </div>
     <h1 v-else>
       An error occurred
