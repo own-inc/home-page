5281ce70aef5c8149d62a2fe0a51a3b60dfd34d3
hanlee <me@hanlee.co>
2020-09-26 20:49:22 +0900

    feat: add header

---
+++ components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <div class="fixed w-full h-16 flex border-b items-center px-6 justify-between">
+      <nuxt-link class="w-20" to="/">
+        <img src="~/assets/logo.svg" alt="Logo">
+      </nuxt-link>
+      <div class="flex items-center">
+        <nuxt-link v-for="item in navItem" :key="item.link" class="text-md px-2 mx-2" :to="item.link">
+          {{ item.title }}
+        </nuxt-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      navItem: [
+        {
+          title: 'Home',
+          link: '/'
+        },
+        {
+          title: 'Services',
+          link: '/services'
+        },
+        {
+          title: 'News',
+          link: '/news'
+        },
+        {
+          title: 'About',
+          link: '/about'
+        },
+        {
+          title: 'Contact',
+          link: '/contact'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
