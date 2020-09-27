43fd3b4c8ebf4b530b9962c3301b39f0c6f18a06
hanlee <me@hanlee.co>
2020-09-28 02:02:11 +0900

    chore: about page

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-center items-center">
+    <h1>About</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+
+<style>
+
+</style>
