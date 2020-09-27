0e4118d58e78d25a549792a79f1103f4f73f1783
hanlee <me@hanlee.co>
2020-09-28 01:59:20 +0900

    feat: page layout

---
+++ layouts/pages.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen overflow-hidden">
+    <Header />
+    <div class="hidden md:block fixed inset-y-0 right-0">
+      <Links />
+    </div>
+    <div class="pt-18 h-full overflow-auto p-6">
+      <page-contant>
+        <Nuxt />
+      </page-contant>
+    </div>
+  </div>
+</template>
