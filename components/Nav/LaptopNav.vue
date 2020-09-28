41eaf0fb641ce307330ed23f3001da10ac040804
hanlee <me@hanlee.co>
2020-09-29 02:17:55 +0900

    fix: change link to home page

--- components/Nav/LaptopNav.vue
+++ components/Nav/LaptopNav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="hidden md:flex space-x-10 px-6">
-    <a href="#services" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
+    <a href="/#services" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
       Services
     </a>
     <nuxt-link to="/" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
