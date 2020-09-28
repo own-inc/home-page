cf51a48db1aa92d9ad14fb75e38421ffa2d14442
hanlee <me@hanlee.co>
2020-09-29 04:39:23 +0900

    feat: add login btn

--- components/Header.vue
+++ components/Header.vue
@@ -16,6 +16,16 @@
             </button>
           </div>
           <laptop-nav :nav-items="navItems" />
+          <div class="hidden md:flex items-center justify-end space-x-8 md:flex-1">
+            <a href="#" class="whitespace-no-wrap text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900">
+              Sign in
+            </a>
+            <span class="inline-flex rounded-md shadow-sm">
+              <a href="#" class="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-teal-400 hover:bg-teal-300 focus:outline-none focus:border-teal-500 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+                Sign up
+              </a>
+            </span>
+          </div>
         </div>
       </div>
       <mobile-nav v-show="showMenu" />
