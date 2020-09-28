26b0a81f033092a00f5cb90884728ffcb6ed7651
hanlee <me@hanlee.co>
2020-09-29 04:24:38 +0900

    Fix: padding top

--- components/PageContant.vue
+++ components/PageContant.vue
@@ -1,6 +1,6 @@
 <template>
   <section :id="$attrs.id" class="flex flex-col items-center">
-    <div class="w-full flex flex-col items-center pt-0">
+    <div class="w-full flex flex-col items-center -mt-6">
       <div class="w-px h-14 bg-teal-300" />
       <div class="w-6 h-6 rounded-full bg-teal-300 -mt-1" />
     </div>
