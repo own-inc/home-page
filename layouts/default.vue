1c84c53fa3586c773286d52646488424fcae2eed
hanlee <me@hanlee.co>
2020-09-29 04:17:35 +0900

    fix: z-index

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="h-screen overflow-hidden">
     <Header />
-    <div class="hidden md:block fixed inset-y-0 right-0">
+    <div class="hidden md:block fixed inset-y-0 right-0 z-40">
       <Links />
     </div>
     <div class="h-full pt-18 md:pt-20 z-0">
-      <div class="h-full overflow-auto p-6">
+      <main class="h-full overflow-auto p-6">
         <Nuxt />
-      </div>
+      </main>
     </div>
   </div>
 </template>
