ce6a33d32d0694a50f196dfd0fa61b6e52620de7
hanlee <me@hanlee.co>
2020-09-29 01:13:03 +0900

    fix: top padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,10 @@
     <div class="hidden md:block fixed inset-y-0 right-0">
       <Links />
     </div>
-    <div class="pt-18 h-full overflow-auto p-6">
-      <Nuxt />
+    <div class="h-full pt-18 md:pt-20 z-0">
+      <div class="h-full overflow-auto p-6">
+        <Nuxt />
+      </div>
     </div>
   </div>
 </template>
