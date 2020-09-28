9ea268fafc8b4e6fb9bcfbd1349ef55afdbd27f0
hanlee <me@hanlee.co>
2020-09-29 01:12:54 +0900

    chore: add id attr & change html tag

--- components/PageContant.vue
+++ components/PageContant.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center">
+  <section :id="$attrs.id" class="flex flex-col items-center">
     <div class="w-full flex flex-col items-center pt-0">
       <div class="w-px h-14 bg-teal-300" />
       <div class="w-6 h-6 rounded-full bg-teal-300 -mt-1" />
@@ -10,7 +10,7 @@
     <div class="w-full mt-6">
       <slot />
     </div>
-  </div>
+  </section>
 </template>
 
 <script lang="ts">
