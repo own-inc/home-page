cf1f51ae5ecf12f25596f6acccc471964058b6e4
hanlee <me@hanlee.co>
2020-09-29 04:16:37 +0900

    feat: change hover color

--- components/Links.vue
+++ components/Links.vue
@@ -1,19 +1,19 @@
 <template>
-  <aside class="flex flex-col pr-8 justify-center h-full z-40">
-    <a class="w-10" href="/">
+  <aside class="flex flex-col pr-8 justify-center h-full space-y-8">
+    <a class="w-10 hover:text-teal-300" href="https://github.com/own-inc" target="_blank">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
         aria-hidden="true"
         focusable="false"
         role="img"
-        class="iconify iconify--bx z-50 hover:text-gray-800"
+        class="iconify iconify--bx z-50"
         preserveAspectRatio="xMidYMid meet"
         viewBox="0 0 24 24"
         style="transform: rotate(360deg);"
       ><path fill-rule="evenodd" clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974c0 4.406 2.857 8.145 6.821 9.465c.499.09.679-.217.679-.481c0-.237-.008-.865-.011-1.696c-2.775.602-3.361-1.338-3.361-1.338c-.452-1.152-1.107-1.459-1.107-1.459c-.905-.619.069-.605.069-.605c1.002.07 1.527 1.028 1.527 1.028c.89 1.524 2.336 1.084 2.902.829c.091-.645.351-1.085.635-1.334c-2.214-.251-4.542-1.107-4.542-4.93c0-1.087.389-1.979 1.024-2.675c-.101-.253-.446-1.268.099-2.64c0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336a9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021c.545 1.372.203 2.387.099 2.64c.64.696 1.024 1.587 1.024 2.675c0 3.833-2.33 4.675-4.552 4.922c.355.308.675.916.675 1.846c0 1.334-.012 2.41-.012 2.737c0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974C22 6.465 17.535 2 12.026 2z" fill="currentColor" /></svg>
     </a>
-    <a class="w-10 mt-8 hover:text-gray-800" href="/">
+    <a class="w-10 hover:text-teal-300" href="/" target="_blank">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -26,7 +26,7 @@
         style="transform: rotate(360deg);"
       ><path d="M19.633 7.997c.013.175.013.349.013.523c0 5.325-4.053 11.461-11.46 11.461c-2.282 0-4.402-.661-6.186-1.809c.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721a4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062c.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973a4.02 4.02 0 0 1-1.771 2.22a8.073 8.073 0 0 0 2.319-.624a8.645 8.645 0 0 1-2.019 2.083z" fill="currentColor" /></svg>
     </a>
-    <a class="w-10 mt-8 hover:text-gray-800" href="/">
+    <a class="w-10 hover:text-teal-300" href="/" target="_blank">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
