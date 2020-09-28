19dcf9510f932e1a730e53c1e9913c5159132983
hanlee <me@hanlee.co>
2020-09-29 02:03:04 +0900

    chore: change to a tag

--- components/Nav/MobileNav.vue
+++ components/Nav/MobileNav.vue
@@ -26,7 +26,7 @@
           </div>
           <div class="my-6">
             <nav class="grid gap-y-8">
-              <nuxt-link class="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150" to="/services">
+              <a class="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150" href="#services">
                 <!-- Heroicon name: chart-bar -->
                 <svg class="flex-shrink-0 h-6 w-6 text-teal-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
@@ -34,7 +34,7 @@
                 <div class="text-base leading-6 font-medium text-gray-900">
                   Services
                 </div>
-              </nuxt-link>
+              </a>
               <a href="#" class="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
                 <!-- Heroicon name: cursor-click -->
                 <svg class="flex-shrink-0 h-6 w-6 text-teal-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
