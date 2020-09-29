9630b78f0f91db6c7092cd580221f5b2d72f6ec8
hanlee <me@hanlee.co>
2020-09-30 04:04:27 +0900

    feat: change text

--- components/NewsletterForm.vue
+++ components/NewsletterForm.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="-mx-6 mt-12 py-8 px-6 text-white">
-    <div class="flex flex-col max-w-screen-md mx-auto">
+    <div class="flex flex-col max-w-screen-sm mx-auto">
       <div class="w-full text-center">
-        <h4 class="text-lg md:text-2xl">
-          OwNのニュースレターを購読して<br>限定情報を手に入れましょう
+        <h4 class="text-xl md:text-3xl font-light">
+          OwN Newsletter
         </h4>
+        <p class="text-gray-300 mt-4">
+          OwNのニュースレターを購読して限定情報を手に入れましょう
+        </p>
       </div>
       <div class="flex flex-col mt-8 text-sm md:text-lg">
         <div class="flex h-12 space-x-6">
@@ -13,7 +16,7 @@
             すぐに購読
           </button>
         </div>
-        <a href="#" class="text-xs self-end mt-4 text-gray-700 hover:text-gray-900">
+        <a href="#" class="text-xs self-end mt-4 text-gray-300 hover:text-gray-500">
           利用規約・プライバシーポリシー
         </a>
       </div>
