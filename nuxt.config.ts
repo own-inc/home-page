b70c33f54e7d88ca0c7f362484a8979940040021
hanlee <me@hanlee.co>
2020-10-01 03:26:40 +0900

    seo: add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,16 @@ import { NuxtConfig } from '@nuxt/types'
 import i18n from './nuxt-i18n.config'
 
 const config: NuxtConfig = {
-  ssr: false,
+  ssr: true,
   target: 'static',
   head: {
     titleTemplate: c => c ? `${c} | OwN Inc.` : 'OwN Inc. - own-auth.io',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'We are a tech company that build Web application, Smartphone Apps, Corporate website or Landing page, we also run our own auth service OwN Auth.' }
+      { hid: 'og:site_name', property: 'og:site_name', content: 'OwN Inc.' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@own_id' }
     ],
     link: [
       { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
@@ -30,6 +32,10 @@ const config: NuxtConfig = {
     '@nuxt/content',
     ['nuxt-i18n', i18n]
   ],
+  env: {
+    DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL || 'https://dev.own-auth.io',
+    URL: process.env.URL || 'https://dev.own-auth.io'
+  },
   content: {
   },
   build: {
