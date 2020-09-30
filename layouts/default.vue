ac6dad5968cafa6f153cc8cb95a6e9b86a734d20
hanlee <me@hanlee.co>
2020-10-01 03:09:01 +0900

    feat: seo & rename component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,55 @@
 <template>
-  <div class="overflow-hidden">
-    <Header />
+  <div>
+    <TheHeader />
     <main class="p-6 pt-18 md:pt-22">
       <Nuxt />
     </main>
-    <Footer />
-    <Links />
+    <TheFooter />
+    <TheLinks />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
+
 export default Vue.extend({
-  head () {
-    return this.$nuxtI18nSeo()
+  head (): MetaInfo {
+    const i18nSeo = this.$nuxtI18nSeo()
+    const { path } = this.$route
+    const pathWithSlash = path.endsWith('/') ? path : `${path}/`
+    const canonical = `${process.env.URL}${pathWithSlash}`
+
+    return {
+      htmlAttrs: i18nSeo.htmlAttrs,
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.$tc('homepage.meta.title')
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.$tc('homepage.meta.description')
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.$tc('homepage.meta.title')
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.$tc('homepage.meta.description')
+        },
+        ...i18nSeo.meta || []
+      ],
+      link: [
+        { rel: 'canonical', href: canonical },
+        ...i18nSeo.link || []
+      ]
+    }
   }
 })
 </script>
