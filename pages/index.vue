f212cb94461242331987444035283943a21e0293
hanlee <me@hanlee.co>
2020-10-02 04:12:17 +0900

    fix: use localepath & icon

--- pages/index.vue
+++ pages/index.vue
@@ -9,21 +9,9 @@
       </h2>
       <div class="inline-flex items-center mt-6">
         <div class="h-px w-8 bg-black mr-4" />
-        <nuxt-link to="/services/" class="inline-flex hover:text-teal-300 items-center">
-          <div class="w-8 mr-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              aria-hidden="true"
-              focusable="false"
-              role="img"
-              class="iconify iconify--bx"
-              preserveAspectRatio="xMidYMid meet"
-              viewBox="0 0 24 24"
-              style="transform: rotate(360deg);"
-            ><path d="M8.465 11.293c1.133-1.133 3.109-1.133 4.242 0l.707.707l1.414-1.414l-.707-.707c-.943-.944-2.199-1.465-3.535-1.465s-2.592.521-3.535 1.465L4.929 12a5.008 5.008 0 0 0 0 7.071a4.983 4.983 0 0 0 3.535 1.462A4.982 4.982 0 0 0 12 19.071l.707-.707l-1.414-1.414l-.707.707a3.007 3.007 0 0 1-4.243 0a3.005 3.005 0 0 1 0-4.243l2.122-2.121z" fill="currentColor" /><path d="M12 4.929l-.707.707l1.414 1.414l.707-.707a3.007 3.007 0 0 1 4.243 0a3.005 3.005 0 0 1 0 4.243l-2.122 2.121c-1.133 1.133-3.109 1.133-4.242 0L10.586 12l-1.414 1.414l.707.707c.943.944 2.199 1.465 3.535 1.465s2.592-.521 3.535-1.465L19.071 12a5.008 5.008 0 0 0 0-7.071a5.006 5.006 0 0 0-7.071 0z" fill="currentColor" /></svg>
-          </div>
-          <span class="relative text-2xl z-10 border-b-2">
+        <nuxt-link :to="localePath('/services/')" class="inline-flex hover:text-teal-300 items-center text-2xl">
+          <Icon icon="bx:bx-link" class="mr-4 text-3xl" />
+          <span class="border-b-2">
             {{ $t('homepage.main.checkService') }}
           </span>
         </nuxt-link>
