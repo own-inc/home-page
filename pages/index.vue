698c903149041ef9c7898e4f279315f5493f242e
hanlee <me@hanlee.co>
2020-10-02 16:38:00 +0900

    fix: english title line height

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="flex flex-col md:flex-row md:w-3/4 relative justify-center mx-auto py-8 md:py-24 xl:py-32 p-4">
     <div class="flex flex-col w-full items-start m-none">
-      <h1 class="text-5xl xl:text-6xl font-semibold">
+      <h1 class="text-5xl xl:text-6xl font-semibold leading-tight">
         {{ $t('homepage.main.title') }}
       </h1>
-      <h2 class="text-lg mt-8 w-2/3">
+      <h2 class="text-lg mt-8 md:w-2/3">
         {{ $t('homepage.main.subtitle') }}
       </h2>
-      <div class="inline-flex items-center mt-6">
+      <div class="inline-flex items-center mt-8">
         <div class="h-px w-8 bg-black mr-4" />
         <nuxt-link :to="localePath('/services/')" class="inline-flex hover:text-teal-300 items-center text-2xl">
           <Icon icon="bx:bx-link" class="mr-4 text-3xl" />
