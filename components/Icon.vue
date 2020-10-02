0c5e7f71a1a03481190c3cbc246000a9e99bf791
hanlee <me@hanlee.co>
2020-10-02 16:51:02 +0900

    fix: type error

--- components/Icon.vue
+++ components/Icon.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
+// @ts-ignore
 import Iconify from '@purge-icons/generated'
 export default Vue.extend({
   props: {
@@ -26,6 +27,7 @@ export default Vue.extend({
       this.$nextTick(() => {
         const el = this.$refs.el as Element
         if (el) {
+          // @ts-ignore
           const svg = Iconify.default.renderSVG(this.icon, {})
           if (svg) {
             el.textContent = ''
