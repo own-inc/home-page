87bf30bb4fa2ec23e263a9059c9c884baed00193
hanlee <me@hanlee.co>
2020-10-02 22:48:10 +0900

    fix: error page min height

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-full">
+  <div class="flex flex-col items-center justify-center min-h-screen">
     <div v-if="error.statusCode === 404" class="animate-appear w-2/3 md:w-1/4 flex justify-center">
       <img src="~/assets/image/rafiki.svg" alt="">
     </div>
-    <h1 v-else>
+    <h1 v-else class="text-3xl font-bold">
       An error occurred
     </h1>
-    <nuxt-link class="btn mt-16" to="/">
+    <nuxt-link class="btn-bk mt-16" to="/">
       Home page
     </nuxt-link>
   </div>
