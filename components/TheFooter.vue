69d8211486146aa53006b57c1b67b9d02b47a6a2
hanlee <me@hanlee.co>
2020-10-02 22:14:28 +0900

    fix: remove footer top margin

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="px-4 md:px-6 mt-8" style="background: linear-gradient(135deg, rgba(1,220,186,1), rgba(127,48,203,1));">
+  <footer class="px-4 md:px-6" style="background: linear-gradient(135deg, rgba(1,220,186,1), rgba(127,48,203,1));">
     <NewsletterForm />
     <div class="w-full border-t-2 border-gray-400 py-2 font-medium text-sm md:text-base text-gray-700">
       Copiright © 2020 OwN. All rights reserved.
