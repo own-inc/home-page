997c13bbad28b95f2d69953aea691e55827a0f6f
hanlee <me@hanlee.co>
2020-10-04 01:13:19 +0900

    chore: nuxt-link name

--- components/lists/newsList.vue
+++ components/lists/newsList.vue
@@ -1,7 +1,7 @@
 <template>
   <table class="table-auto text-xs md:text-lg mx-auto">
     <tbody class="divide-y divide-gray-400">
-      <NuxtLink v-for="item in news" :key="item.slug" :to="localePath({ name: 'news-slug', params: { slug: item.slug } })" tag="tr" class="hover:text-gray-600 cursor-pointer">
+      <nuxt-link v-for="item in news" :key="item.slug" :to="localePath({ name: 'news-slug', params: { slug: item.slug } })" tag="tr" class="hover:text-gray-600 cursor-pointer">
         <td class="p-2 md:p-4">
           {{ $d(new Date(item.createdAt), 'dateTime') }}
         </td>
@@ -13,7 +13,7 @@
         <td class="p-4p-2 md:p-4 w-3/5">
           {{ item.title }}
         </td>
-      </NuxtLink>
+      </nuxt-link>
     </tbody>
   </table>
 </template>
