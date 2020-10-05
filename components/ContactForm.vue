fa51bb5e11adcb2f4044f9331d19931f4008f798
hanlee <me@hanlee.co>
2020-10-06 05:45:41 +0900

    feat: contact form api

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,11 +1,20 @@
 <template>
-  <form class="w-full max-w-lg bg-white border-2 border-gray-100 shadow-lg p-6 rounded-lg z-20">
+  <div class="w-full max-w-lg bg-white border-2 border-gray-100 shadow-lg p-6 rounded-lg z-20">
+    <div v-if="show" class="flex flex-wrap mb-6">
+      <div class="text-2xl font-semibold">
+        お問い合わせありがとうございました!
+      </div>
+      <p class="mt-2">
+        以下の内容でお問い合わせを受付いたしました。
+        2営業日以内に担当よりご連絡致しますので今しばらくお待ちくださいませ。
+      </p>
+    </div>
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full px-3">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
           {{ $t('contact-page.form.name') }}
         </label>
-        <input id="nick" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text">
+        <input v-model="form.name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" :disabled="isDisable">
         <p class="text-gray-600 text-xs italic">
           {{ $t('contact-page.form.require') }}
         </p>
@@ -16,7 +25,7 @@
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
           {{ $t('contact-page.form.email') }}
         </label>
-        <input id="email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="email">
+        <input v-model="form.email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="email" :disabled="isDisable">
         <p class="text-gray-600 text-xs italic">
           {{ $t('contact-page.form.require') }}
         </p>
@@ -27,23 +36,42 @@
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
           {{ $t('contact-page.form.message') }}
         </label>
-        <textarea id="message" class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" />
+        <textarea v-model="form.message" class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" :disabled="isDisable" />
       </div>
     </div>
     <div class="md:flex md:items-center">
       <div class="md:w-1/3">
-        <TealButton>
+        <TealButton @click="uploadFormData()">
           {{ $t('contact-page.form.send') }}
         </TealButton>
       </div>
       <div class="md:w-2/3" />
     </div>
-  </form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      form: {
+        name: '',
+        email: '',
+        message: ''
+      },
+      show: false,
+      isDisable: false
+    }
+  },
+  methods: {
+    async uploadFormData () {
+      const proxy = 'https://cors-anywhere.herokuapp.com/'
+      const url = 'https://hooks.zapier.com/hooks/catch/8656224/ogh0to4/'
+      await this.$axios.$post(proxy + url, this.form)
+      this.show = true
+      this.isDisable = true
+    }
+  }
 }
 </script>
 
--- components/TealButton.vue
+++ components/TealButton.vue
@@ -1,12 +1,16 @@
 <template>
-  <button class="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-teal-400 hover:bg-teal-300 focus:outline-none focus:border-teal-500 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+  <button class="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-teal-400 hover:bg-teal-300 focus:outline-none focus:border-teal-500 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150" @click="doClick">
     <slot />
   </button>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    doClick () {
+      this.$emit('click')
+    }
+  }
 }
 </script>
 
