0a60abc912508d29246117db856da2e898eb3437
hanlee <me@hanlee.co>
2020-10-06 17:38:33 +0900

    feat: remove @nuxtjs/netlify-files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,8 +57,7 @@ const config: NuxtConfig = {
     '@nuxt/typescript-build',
     '@nuxtjs/svg',
     '@nuxtjs/tailwindcss',
-    'nuxt-purge-icons-module',
-    '@nuxtjs/netlify-files'
+    'nuxt-purge-icons-module'
   ],
   modules: [
     '@nuxt/content',
@@ -108,22 +107,6 @@ const config: NuxtConfig = {
     hostname: process.env.URL || 'https://dev.own-auth.io',
     gzip: true,
     i18n: true
-  },
-  netlifyFiles: {
-    headers: [
-      {
-        for: '/*',
-        values: { 'X-XSS-Protection': '1; mode=block' }
-      }
-    ],
-    redirects: [
-      {
-        from: '/contactform',
-        to: 'https://hooks.zapier.com/hooks/catch/8656224/ogh0to4/',
-        status: 200,
-        force: true
-      }
-    ]
   }
 }
 
