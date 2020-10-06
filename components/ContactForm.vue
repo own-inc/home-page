cdb168515069cdd286c0b8cfc2ec3644d53d3d64
hanlee <me@hanlee.co>
2020-10-07 05:21:05 +0900

    fix: bt ndisable

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,54 +1,56 @@
 <template>
-  <ValidationObserver v-slot="{ invalid }" class="w-full max-w-lg bg-white border-2 border-gray-100 shadow-lg p-6 rounded-lg z-20" tag="form" @submit.prevent="onSubmit()">
-    <div v-if="show" class="flex flex-wrap mb-6">
-      <div class="text-2xl font-semibold">
-        {{ $t('contact-page.form.thanks') }}
+  <div>
+    <ValidationObserver v-slot="{ invalid }" class="w-full max-w-lg bg-white border-2 border-gray-100 shadow-lg p-6 rounded-lg z-20" tag="form" @submit.prevent="onSubmit()">
+      <div v-if="show" class="flex flex-wrap mb-6">
+        <div class="text-2xl font-semibold">
+          {{ $t('contact-page.form.thanks') }}
+        </div>
+        <i18n path="contact-page.form.thanks-message" tag="p" class="mt-2">
+          <template #br>
+            <br>
+          </template>
+        </i18n>
       </div>
-      <i18n path="contact-page.form.thanks-message" tag="p" class="mt-2">
-        <template #br>
-          <br>
-        </template>
-      </i18n>
-    </div>
-    <ValidationProvider v-slot="{ errors }" :name="$t('contact-page.form.name')" rules="required" class="flex flex-wrap -mx-3 mb-4" tag="div">
-      <div class="w-full px-3">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-          {{ $t('contact-page.form.name') }}
-        </label>
-        <input v-model="form.name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" :disabled="isDisable">
-        <p class="text-red-600 text-xs italic h-4">
-          {{ errors[0] }}
-        </p>
-      </div>
-    </ValidationProvider>
-    <ValidationProvider v-slot="{ errors }" :name="$t('contact-page.form.email')" rules="required|email" class="flex flex-wrap -mx-3 mb-4" tag="div">
-      <div class="w-full px-3">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-          {{ $t('contact-page.form.email') }}
-        </label>
-        <input v-model="form.email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="email" :disabled="isDisable">
-        <p class="text-red-600 text-xs italic h-4">
-          {{ errors[0] }}
-        </p>
-      </div>
-    </ValidationProvider>
-    <ValidationProvider v-slot="{ errors }" :name="$t('contact-page.form.message')" rules="required" class="flex flex-wrap -mx-3 mb-4" tag="div">
-      <div class="w-full px-3">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-          {{ $t('contact-page.form.message') }}
-        </label>
-        <textarea v-model="form.message" class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" :disabled="isDisable" />
-        <p class="text-red-600 text-xs italic h-4">
-          {{ errors[0] }}
-        </p>
-      </div>
-    </ValidationProvider>
-    <TealButton v-if="!isDisable" :is-disable="invalid" type="submit">
-      <Icon v-if="loading" class="iconify mr-2 -ml-1 animate-spin" icon="mdi:loading" />
-      <Icon v-else icon="bx:bx-send" class="mr-2" />
-      {{ $t('contact-page.form.send') }}
-    </TealButton>
-  </ValidationObserver>
+      <ValidationProvider v-slot="{ errors }" :name="$t('contact-page.form.name')" rules="required" class="flex flex-wrap -mx-3 mb-4" tag="div">
+        <div class="w-full px-3">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+            {{ $t('contact-page.form.name') }}
+          </label>
+          <input v-model="form.name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" :disabled="isDisable">
+          <p class="text-red-600 text-xs italic h-4">
+            {{ errors[0] }}
+          </p>
+        </div>
+      </ValidationProvider>
+      <ValidationProvider v-slot="{ errors }" :name="$t('contact-page.form.email')" rules="required|email" class="flex flex-wrap -mx-3 mb-4" tag="div">
+        <div class="w-full px-3">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+            {{ $t('contact-page.form.email') }}
+          </label>
+          <input v-model="form.email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="email" :disabled="isDisable">
+          <p class="text-red-600 text-xs italic h-4">
+            {{ errors[0] }}
+          </p>
+        </div>
+      </ValidationProvider>
+      <ValidationProvider v-slot="{ errors }" :name="$t('contact-page.form.message')" rules="required" class="flex flex-wrap -mx-3 mb-4" tag="div">
+        <div class="w-full px-3">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+            {{ $t('contact-page.form.message') }}
+          </label>
+          <textarea v-model="form.message" class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" :disabled="isDisable" />
+          <p class="text-red-600 text-xs italic h-4">
+            {{ errors[0] }}
+          </p>
+        </div>
+      </ValidationProvider>
+      <TealButton v-if="!isDisable" :is-disable="invalid" type="submit">
+        <Icon v-if="loading" class="iconify mr-2 -ml-1 animate-spin" icon="mdi:loading" />
+        <Icon v-else icon="bx:bx-send" class="mr-2" />
+        {{ $t('contact-page.form.send') }}
+      </TealButton>
+    </ValidationObserver>
+  </div>
 </template>
 
 <script lang="ts">
