8dd01eefa526f13f7d9131cb4b617004b6e1dfc4
hanlee <me@hanlee.co>
2020-10-08 00:39:04 +0900

    feat: resize font & use date in mobile

--- components/lists/newsList.vue
+++ components/lists/newsList.vue
@@ -1,16 +1,19 @@
 <template>
-  <table class="table-auto text-xs md:text-lg mx-auto">
+  <table class="table-auto text-sm md:text-lg mx-auto">
     <tbody class="divide-y divide-gray-400">
-      <nuxt-link v-for="item in news" :key="item.slug" :to="localePath({ name: 'news-slug', params: { slug: item.slug } })" tag="tr" class="hover:text-gray-600 cursor-pointer">
-        <td class="p-2 md:p-4">
+      <nuxt-link v-for="item in news" :key="item.slug" :to="localePath({ name: 'news-slug', params: { slug: item.slug } })" tag="tr" class="hover:text-gray-600 cursor-pointer my-2">
+        <td class="hidden md:block px-4 py-6  whitespace-no-wrap">
           {{ $d(new Date(item.createdAt), 'dateTime') }}
         </td>
+        <td class="block md:hidden px-2 py-4  whitespace-no-wrap">
+          {{ $d(new Date(item.createdAt), 'date') }}
+        </td>
         <td class="p-2 md:p-4 whitespace-no-wrap">
-          <span class="px-2 py-1 md:px-3 md:py-2 border rounded-full border-teal-400 md:text-base">
+          <span class="px-2 py-1 md:px-3 md:py-2 border rounded-full border-teal-400 text-xs md:text-base">
             {{ item.type }}
           </span>
         </td>
-        <td class="p-4p-2 md:p-4 w-3/5">
+        <td class="p-2 md:p-4">
           {{ item.title }}
         </td>
       </nuxt-link>
