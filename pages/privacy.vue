872672017a448b31162e736eb6df61f36438c2a6
hanlee <me@hanlee.co>
2020-10-08 00:17:17 +0900

    feat: add privacy policy page

---
+++ pages/privacy.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container lg:max-w-4xl mx-auto py-6 md:py-12 min-h-screen px-4">
+    <a href="#" class="inline-flex items-center space-x-2 text-gray-600 hover:text-gray-900" @click.prevent="goBack()">
+      <Icon icon="bx:bx-arrow-back" />
+      <p>{{ $t('privacy-page.go-back') }}</p>
+    </a>
+    <h1 class="text-2xl md:text-4xl text-teal-500 mt-4 md:mt-8">
+      {{ news.title }}
+    </h1>
+    <div class="mt-4 md:mt-8">
+      <NuxtContent
+        class="prose prose-sm sm:prose lg:prose-lg"
+        :document="news"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
+
+export default Vue.extend({
+  async asyncData ({ $content, app, error }) {
+    const path = `/${app.i18n.locale}/privacy`
+    let news
+
+    try {
+      news = await $content(path).fetch()
+    } catch (e) {
+      return error({ statusCode: 404, message: 'Page not found' })
+    }
+
+    return {
+      news
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/')
+    }
+  },
+  head (): MetaInfo {
+    const title = (this as any).news.title
+    const description = (this as any).news.description
+
+    return {
+      title: (this as any).news.title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+        // Open Graph
+        { hid: 'og:title', property: 'og:title', content: title },
+        { hid: 'og:description', property: 'og:description', content: description },
+        // Twitter Card
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
+        { hid: 'twitter:description', name: 'twitter:description', content: description }
+      ]
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
